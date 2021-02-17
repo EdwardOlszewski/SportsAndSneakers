066e214e3594dbeb609eb8f2c236dafee4bebbb6
EdwardOlszewski <edolszewski96@gmail.com>
2021-02-16 20:17:56 -0600

    2/16 improvments

---
+++ backend/controllers/youtubeController.js
@@ -0,0 +1,10 @@
+import asynchHandler from 'express-async-handler'
+
+// @desc    Fetch all youtube videos in the channel
+// @route   GET /api/videos
+// @access  Public
+const getYoutubeVideos = asynchHandler(async (req, res) => {
+    const 
+})
+
+export { getYoutubeVideos }
--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,27 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import morgan from 'morgan'
 
+import youtubeRoutes from './routes/youtubeRoutes.js'
+
+// Init dotenv
 dotenv.config()
+
+// Connect DB
+connectDB()
+
+// Init app with express
 const app = express()
 
+// Use morgan while in development
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
+// Init json
 app.use(express.json())
+
+// Routes
+app.use('/api/videos', youtubeRoutes)
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
