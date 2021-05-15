63cb945b925b44997c5b55e2e2175cf37189556b
EdwardOlszewski <edolszewski96@gmail.com>
2021-05-14 21:09:41 -0500

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-// Header
+// Headers
 import Header from './components/Header'
 // User Screens
 import HomeScreen from './userScreens/HomeScreen'
